78a4e8e0863df8e15ee4eb5b9a7224a5caac42fd
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-10-06 23:46:51 -0400

    Q652

--- src/leetcode/editor/cn/Q652_FindDuplicateSubtrees.java
+++ src/leetcode/editor/cn/Q652_FindDuplicateSubtrees.java
@@ -30,34 +30,61 @@
 
 package leetcode.editor.cn;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Q652_FindDuplicateSubtrees{
+public class Q652_FindDuplicateSubtrees {
     public static void main(String[] args) {
         Q652_FindDuplicateSubtrees tmp = new Q652_FindDuplicateSubtrees();
         Solution solution = tmp.new Solution();
     }
 
-     // Definition for a binary tree node.
-     public class TreeNode {
-         int val;
-         TreeNode left;
-         TreeNode right;
-         TreeNode() {}
-         TreeNode(int val) { this.val = val; }
-         TreeNode(int val, TreeNode left, TreeNode right) {
-             this.val = val;
-             this.left = left;
-             this.right = right;
-         }
-     }
-    //leetcode submit region begin(Prohibit modification and deletion)
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-class Solution {
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        TreeNode() {
+        }
 
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        int t;
+        Map<String, Integer> trees;
+        Map<Integer, Integer> count;
+        List<TreeNode> ans;
 
+        public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+            t = 1;
+            trees = new HashMap();
+            count = new HashMap();
+            ans = new ArrayList();
+            lookup(root);
+            return ans;
+        }
+
+        public int lookup(TreeNode node) {
+            if (node == null) return 0;
+            String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
+            int uid = trees.computeIfAbsent(serial, x -> t++);
+            count.put(uid, count.getOrDefault(uid, 0) + 1);
+            if (count.get(uid) == 2)
+                ans.add(node);
+            return uid;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
 }
