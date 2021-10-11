0758772a29e8ae7723854b3722eaa3b9eea41903
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-10-10 22:46:35 -0400

    add questions until today

---
+++ src/leetcode/editor/cn/Q273_IntegerToEnglishWords.java
@@ -0,0 +1,57 @@
+//将非负整数 num 转换为其对应的英文表示。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：num = 123
+//输出："One Hundred Twenty Three"
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：num = 12345
+//输出："Twelve Thousand Three Hundred Forty Five"
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：num = 1234567
+//输出："One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：num = 1234567891
+//输出："One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven 
+//Thousand Eight Hundred Ninety One"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= num <= 2³¹ - 1 
+// 
+// Related Topics 递归 数学 字符串 👍 164 👎 0
+
+package leetcode.editor.cn;
+public class Q273_IntegerToEnglishWords{
+    public static void main(String[] args) {
+        Q273_IntegerToEnglishWords tmp = new Q273_IntegerToEnglishWords();
+        Solution solution = tmp.new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String numberToWords(int num) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
