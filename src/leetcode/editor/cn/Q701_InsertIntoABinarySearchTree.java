3b9ec75868851138b93f73d6a0866139728a0a17
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-10-12 21:29:56 -0400

    Q701 finished

--- src/leetcode/editor/cn/Q701_InsertIntoABinarySearchTree.java
+++ src/leetcode/editor/cn/Q701_InsertIntoABinarySearchTree.java
@@ -48,25 +48,33 @@ public class Q701_InsertIntoABinarySearchTree{
         Q701_InsertIntoABinarySearchTree tmp = new Q701_InsertIntoABinarySearchTree();
         Solution solution = tmp.new Solution();
     }
+
+     // Definition for a binary tree node.
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-
+        if(root == null) return new TreeNode(val);
+        if(root.val < val){
+            root.right = insertIntoBST(root.right, val);
+        }
+        if(root.val > val){
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
