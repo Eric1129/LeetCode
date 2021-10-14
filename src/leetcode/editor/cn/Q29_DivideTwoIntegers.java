4b8040a181caf3d46cf5fd0d877548f656685245
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-10-14 00:12:28 -0400

    finish Q29

--- src/leetcode/editor/cn/Q29_DivideTwoIntegers.java
+++ src/leetcode/editor/cn/Q29_DivideTwoIntegers.java
@@ -72,24 +72,67 @@ class Solution {
             rev = !rev;
         }
 
-        List<Integer> candidates = new ArrayList<Integer>();
-        candidates.add(divisor);
-        int index = 0;
-        // 注意溢出
-        while (candidates.get(index) >= dividend - candidates.get(index)) {
-            candidates.add(candidates.get(index) + candidates.get(index));
-            ++index;
-        }
-        int ans = 0;
-        for (int i = candidates.size() - 1; i >= 0; --i) {
-            if (candidates.get(i) >= dividend) {
-                ans += 1 << i;
-                dividend -= candidates.get(i);
+        int left = 1, right = Integer.MAX_VALUE, ans = 0;
+        while (left <= right) {
+            // 注意溢出，并且不能使用除法
+            int mid = left + ((right - left) >> 1);
+            boolean check = quickAdd(divisor, mid, dividend);
+            if (check) {
+                ans = mid;
+                // 注意溢出
+                if (mid == Integer.MAX_VALUE) {
+                    break;
+                }
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
+//        Method 2
+//        List<Integer> candidates = new ArrayList<Integer>();
+//        candidates.add(divisor);
+//        int index = 0;
+//        // 注意溢出
+//        while (candidates.get(index) >= dividend - candidates.get(index)) {
+//            candidates.add(candidates.get(index) + candidates.get(index));
+//            ++index;
+//        }
+//        int ans = 0;
+//        for (int i = candidates.size() - 1; i >= 0; --i) {
+//            if (candidates.get(i) >= dividend) {
+//                ans += 1 << i;
+//                dividend -= candidates.get(i);
+//            }
+//        }
+
         return rev ? -ans : ans;
     }
+        // 快速乘
+        public boolean quickAdd(int y, int z, int x) {
+            // x 和 y 是负数，z 是正数
+            // 需要判断 z * y >= x 是否成立
+            int result = 0, add = y;
+            while (z != 0) {
+                if ((z & 1) != 0) {
+                    // 需要保证 result + add >= x
+                    if (result < x - add) {
+                        return false;
+                    }
+                    result += add;
+                }
+                if (z != 1) {
+                    // 需要保证 add + add >= x
+                    if (add < x - add) {
+                        return false;
+                    }
+                    add += add;
+                }
+                // 不能使用除法
+                z >>= 1;
+            }
+            return true;
+        }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
