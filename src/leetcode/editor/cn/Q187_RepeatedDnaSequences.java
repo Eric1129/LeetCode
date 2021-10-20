c03d039237eafe702b871475ff71c13e9763b1c0
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-10-19 21:54:45 -0400

    finish Q187

--- src/leetcode/editor/cn/Q187_RepeatedDnaSequences.java
+++ src/leetcode/editor/cn/Q187_RepeatedDnaSequences.java
@@ -30,6 +30,12 @@
 // Related Topics 位运算 哈希表 字符串 滑动窗口 哈希函数 滚动哈希 👍 286 👎 0
 
 package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Q187_RepeatedDnaSequences{
     public static void main(String[] args) {
         Q187_RepeatedDnaSequences tmp = new Q187_RepeatedDnaSequences();
@@ -38,7 +44,19 @@ public class Q187_RepeatedDnaSequences{
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-
+        List<String> ans = new ArrayList<>();
+        Map<String,Integer> map = new HashMap<>();
+        if(s.length() < 10) return ans;
+        int n = s.length();
+        char[] subString = new char[10];
+        for(int i = 0; i <= n-10; i++){
+            String sub = s.substring(i, i+10);
+            map.put(sub, map.getOrDefault(sub,0)+1);
+            if(map.get(sub) == 2){
+                ans.add(sub);
+            }
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
