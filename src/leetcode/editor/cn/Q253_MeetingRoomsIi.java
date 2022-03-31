a2702ef055a9fa737bc0df8e1da99bc98433b25c
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2022-03-31 13:58:35 -0400

    Finish Q253_MeetingRoomsIi

---
+++ src/leetcode/editor/cn/Q253_MeetingRoomsIi.java
@@ -0,0 +1,59 @@
+//给你一个会议时间安排的数组 intervals ，每个会议时间都会包括开始和结束的时间 intervals[i] = [starti, endi] ，返回 
+//所需会议室的最小数量 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：intervals = [[0,30],[5,10],[15,20]]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：intervals = [[7,10],[2,4]]
+//输出：1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= intervals.length <= 10⁴ 
+// 0 <= starti < endi <= 10⁶ 
+// 
+// Related Topics 贪心 数组 双指针 排序 堆（优先队列） 👍 418 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Q253_MeetingRoomsIi{
+    public static void main(String[] args) {
+        Q253_MeetingRoomsIi tmp = new Q253_MeetingRoomsIi();
+        Solution solution = tmp.new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if (intervals==null || intervals.length == 0) return 0;
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(intervals[0][1]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int last = queue.peek();
+            if (last <= intervals[i][0]) queue.poll();
+            queue.offer(intervals[i][1]);
+        }
+        return queue.size();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/Q78_Subsets.java
@@ -0,0 +1,46 @@
+//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。 
+//
+// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0]
+//输出：[[],[0]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10 
+// -10 <= nums[i] <= 10 
+// nums 中的所有元素 互不相同 
+// 
+// Related Topics 位运算 数组 回溯 👍 1553 👎 0
+
+package leetcode.editor.cn;
+public class Q78_Subsets{
+    public static void main(String[] args) {
+        Q78_Subsets tmp = new Q78_Subsets();
+        Solution solution = tmp.new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
