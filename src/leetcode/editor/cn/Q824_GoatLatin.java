50f89e6a4feba76ff4644784382a0675dafef8ae
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2022-04-12 17:45:21 -0400

    Finish Q824_GoatLatin

---
+++ src/leetcode/editor/cn/Q824_GoatLatin.java
@@ -0,0 +1,77 @@
+//给定一个由空格分割单词的句子 S。每个单词只包含大写或小写字母。 
+//
+// 我们要将句子转换为 “Goat Latin”（一种类似于 猪拉丁文 - Pig Latin 的虚构语言）。 
+//
+// 山羊拉丁文的规则如下： 
+//
+// 
+// 如果单词以元音开头（a, e, i, o, u），在单词后添加"ma"。 
+// 例如，单词"apple"变为"applema"。 
+// 
+// 如果单词以辅音字母开头（即非元音字母），移除第一个字符并将它放到末尾，之后再添加"ma"。 
+// 例如，单词"goat"变为"oatgma"。 
+// 
+// 根据单词在句子中的索引，在单词最后添加与索引相同数量的字母'a'，索引从1开始。 
+// 例如，在第一个单词后添加"a"，在第二个单词后添加"aa"，以此类推。 
+// 
+//
+// 返回将 S 转换为山羊拉丁文后的句子。 
+//
+// 示例 1: 
+//
+// 
+//输入: "I speak Goat Latin"
+//输出: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: "The quick brown fox jumped over the lazy dog"
+//输出: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa 
+//hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+// 
+//
+// 说明: 
+//
+// 
+// S 中仅包含大小写字母和空格。单词间有且仅有一个空格。 
+// 1 <= S.length <= 150。 
+// 
+// Related Topics 字符串 👍 59 👎 0
+
+package leetcode.editor.cn;
+public class Q824_GoatLatin{
+    public static void main(String[] args) {
+        Q824_GoatLatin tmp = new Q824_GoatLatin();
+        Solution solution = tmp.new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String toGoatLatin(String sentence) {
+        String[] tmp = sentence.split(" ");
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < tmp.length; i++) {
+            StringBuilder sb = new StringBuilder(tmp[i]);
+            if (!( sb.charAt(0) == 'a' || sb.charAt(0) == 'e' || sb.charAt(0) == 'i'
+                    ||sb.charAt(0) == 'o' ||sb.charAt(0) == 'u' ||
+                    sb.charAt(0) == 'A' || sb.charAt(0) == 'E' || sb.charAt(0) == 'I'
+                    ||sb.charAt(0) == 'O' ||sb.charAt(0) == 'U' )) {
+                char c = sb.charAt(0);
+                sb.deleteCharAt(0);
+                sb.append(c);
+            }
+            sb.append("ma");
+            for (int j = 0; j <= i; j++){
+                sb.append("a");
+            }
+            tmp[i] = sb.toString();
+            ans.append(tmp[i]+" ");
+        }
+        ans.deleteCharAt(ans.length()-1);
+        return ans.toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
