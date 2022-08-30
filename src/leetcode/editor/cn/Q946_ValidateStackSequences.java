78dbc73fa8aac37010d6f327d155ba8c65f44dad
Eric1129 <eric.11.29@outlook.com>
2022-08-30 19:03:06 -0400

    finish Q946_ValidateStackSequences

---
+++ src/leetcode/editor/cn/Q946_ValidateStackSequences.java
@@ -0,0 +1,79 @@
+////////给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作
+//序列
+////的结
+//////果时
+////////，返回 true；否则，返回 false 。 
+////////
+//////// 
+////////
+//////// 示例 1： 
+////////
+//////// 
+////////输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+////////输出：true
+////////解释：我们可以按以下顺序执行：
+////////push(1), push(2), push(3), push(4), pop() -> 4,
+////////push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+//////// 
+////////
+//////// 示例 2： 
+////////
+//////// 
+////////输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+////////输出：false
+////////解释：1 不能在 2 之前弹出。
+//////// 
+////////
+//////// 
+////////
+//////// 提示： 
+////////
+//////// 
+//////// 1 <= pushed.length <= 1000 
+//////// 0 <= pushed[i] <= 1000 
+//////// pushed 的所有元素 互不相同 
+//////// popped.length == pushed.length 
+//////// popped 是 pushed 的一个排列 
+//////// 
+//////// Related Topics栈 | 数组 | 模拟 
+////////
+//////// 👍 262, 👎 0 
+////////
+////////
+////////
+////////
+//////
+////
+//
+
+package leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class Q946_ValidateStackSequences{
+    public static void main(String[] args) {
+        Q946_ValidateStackSequences tmp = new Q946_ValidateStackSequences();
+        Solution solution = tmp.new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int n = popped.length;
+        for (int i = 0, j = 0; i < n; i++) {
+            stack.push(pushed[i]);
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
