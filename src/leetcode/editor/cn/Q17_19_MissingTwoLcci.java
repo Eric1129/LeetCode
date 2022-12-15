2b3f2505e44596436c01bcc26e41284544302d1f
Eric1129 <eric.11.29@outlook.com>
2022-12-15 16:13:48 -0500

    finish Q17.19

---
+++ src/leetcode/editor/cn/Q17_19_MissingTwoLcci.java
@@ -0,0 +1,43 @@
+//给定一个数组，包含从 1 到 N 所有的整数，但其中缺了两个数字。你能在 O(N) 时间内只用 O(1) 的空间找到它们吗？ 
+//
+// 以任意顺序返回这两个数字均可。 
+//
+// 示例 1: 
+//
+// 输入: [1]
+//输出: [2,3] 
+//
+// 示例 2: 
+//
+// 输入: [2,3]
+//输出: [1,4] 
+//
+// 提示： 
+//
+// 
+// nums.length <= 30000 
+// 
+//
+// Related Topics位运算 | 数组 | 哈希表 
+//
+// 👍 73, 👎 0 
+//
+//
+//
+//
+
+package leetcode.editor.cn;
+public class Q17_19_MissingTwoLcci{
+    public static void main(String[] args) {
+        Q17_19_MissingTwoLcci tmp = new Q17_19_MissingTwoLcci();
+        Solution solution = tmp.new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] missingTwo(int[] nums) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
